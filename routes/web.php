2282adf0cbcc3815898a4f7d1b1a60008dba3302
SeviolaIS <seviolasafitri939@gmail.com>
2026-05-12 09:57:28 +0700

    kwitansi

--- routes/web.php
+++ routes/web.php
@@ -44,6 +44,7 @@ Route::middleware('auth')->group(function () {
         Route::post('/kasir/checkout', [PosController::class, 'checkout']);
         Route::get('/kasir/receipt/{id}', [PosController::class, 'receipt']);
         Route::get('/kasir/setor', [PosController::class, 'setor']);
+        Route::get('/kasir/receipt-pdf/{id}', [PosController::class, 'receiptPdf']);
 
         // ===== RESTOCK =====
         Route::get('/restock/history', [RestockController::class, 'history'])->name('restock.history');
@@ -80,6 +81,7 @@ Route::middleware('auth')->group(function () {
         Route::get('/kasir', [PosController::class, 'index'])->name('kasir.index');
         Route::post('/kasir/checkout', [PosController::class, 'checkout']);
         Route::get('/kasir/receipt/{id}', [PosController::class, 'receipt']);
+        Route::get('/kasir/receipt-pdf/{id}', [PosController::class, 'receiptPdf']);
         Route::post('/kasir/setor', [PosController::class, 'setor'])->name('kasir.setor');
     });
 
@@ -89,6 +91,7 @@ Route::middleware('auth')->group(function () {
         Route::get('/products/{product}', [ProductController::class, 'show'])->name('products.show');
         Route::get('/report/setoran', [ReportController::class, 'depositReport']);
         Route::get('/classroom', [ClassRoomController::class, 'index'])->name('classroom.index');
+        Route::get('/report/transaksi', [PosController::class, 'dailyReport'])->name('report.transaksi');
     });
 
     // ================= USER =================
